e924cd7450d2b8c4c1ac21b5a2d11c8bc0fa7023
William Nam-Amnath <william.nam-amnath@mail.mcgill.ca>
2024-08-11 11:51:51 -0400

    Created website template

--- client/src/pages/components/Navbar.js
+++ client/src/pages/components/Navbar.js
@@ -1,39 +1,22 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
 
-import { LoginInfoContext } from "../UserAccount/LoginInfo";
 
 const Navbar = () => {
 
-  const { signedOut, loginConfirmed } = useContext(LoginInfoContext);
 
     return (
         <>
           <nav>
             <Link to="/">
-              <h1>NUTRISSENCE</h1>
+              <h1>NEUROFIBROMATOSIS</h1>
             </Link>
             <ul>
-              {loginConfirmed && (
-              <Link to="/favorites" className="navLink">
-                <li>My Favorites</li>
-              </Link>
-              )}
               <Link to="/contact" className="navLink">
-                <li>Contact</li>
+                <li>Contact Us</li>
               </Link>
               <Link to="/about" className="navLink">
                 <li>About</li>
               </Link>
-              {!loginConfirmed ? (
-              <Link to="/login" className="navLink">
-                <li>Login | Signup</li>
-              </Link>
-              ) : (
-              <Link to="/login" className="navLink">
-                <li onClick={signedOut} style={{backgroundColor:"white", padding: "5px", color: "black", borderRadius:"5px"}}>Logout</li>
-              </Link>
-              )}
             </ul>
           </nav>
         </>
